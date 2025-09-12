c9a273e1802e028e518f97b9bbe4833cbb3da5d3
kelsonwinith <kelsonwinith@gmail.com>
2025-09-12 11:38:31 +0700

    init

---
+++ src/main/java/com/example/realtimeleaderboard/RealtimeleaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimeleaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeleaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeleaderboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realtimeleaderboard/RealtimeleaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimeleaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeleaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
